d15e9574b2ef9eb5e2ea9eb0444976fa337e9016
cjacobme <cjacobme@aol.com>
2023-08-11 22:18:45 +0200

    added Solution

---
+++ src/main/java/cj/software/genetics/schedule/entity/Solution.java
@@ -0,0 +1,52 @@
+package cj.software.genetics.schedule.entity;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private final List<Worker> workers = new ArrayList<>();
+
+    private Solution() {
+    }
+
+    public List<Worker> getWorkers() {
+        return Collections.unmodifiableList(workers);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public void addWorker(Worker worker) {
+        workers.add(worker);
+    }
+
+    public static class Builder {
+        protected Solution instance;
+
+        protected Builder() {
+            instance = new Solution();
+        }
+
+        public Solution build() {
+            Solution result = instance;
+            instance = null;
+            return result;
+        }
+
+        public Builder withWorkers(Worker... workers) {
+            instance.workers.clear();
+            if (workers != null) {
+                instance.workers.addAll(Arrays.asList(workers));
+            }
+            return this;
+        }
+    }
+}
---
+++ src/test/java/cj/software/genetics/schedule/entity/SolutionTest.java
@@ -0,0 +1,76 @@
+package cj.software.genetics.schedule.entity;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+class SolutionTest {
+
+    @Test
+    void implementsSerializable() {
+        Class<?>[] interfaces = Solution.class.getInterfaces();
+        assertThat(interfaces).as("interfaces").contains(Serializable.class);
+    }
+
+    @Test
+    void constructEmpty()
+            throws NoSuchFieldException,
+            SecurityException,
+            IllegalArgumentException,
+            IllegalAccessException {
+        Solution.Builder builder = Solution.builder();
+        assertThat(builder).as("builder").isNotNull();
+
+        Field field = builder.getClass().getDeclaredField("instance");
+
+        Object instanceBefore = field.get(builder);
+        assertThat(instanceBefore).as("instance in builder before build").isNotNull().isInstanceOf(
+                Solution.class);
+
+        Solution instance = builder.build();
+        assertThat(instance).as("built instance").isNotNull();
+
+        Object instanceAfter = field.get(builder);
+        assertThat(instanceAfter).as("instance in builder after build").isNull();
+        SoftAssertions softy = new SoftAssertions();
+        softy.assertThat(instance.getWorkers()).as("workers").isEmpty();
+        softy.assertAll();
+    }
+
+    @Test
+    void constructFilled() {
+        Worker worker1 = mock(Worker.class);
+        Worker worker2 = mock(Worker.class);
+        Worker worker3 = mock(Worker.class);
+        Solution instance = Solution.builder()
+                .withWorkers(worker1, worker2, worker3)
+                .build();
+        assertThat(instance).as("built instance").isNotNull();
+        SoftAssertions softy = new SoftAssertions();
+        softy.assertThat(instance.getWorkers()).as("workers").containsExactly(worker1, worker2, worker3);
+        softy.assertAll();
+        Worker worker4 = mock(Worker.class);
+        instance.addWorker(worker4);
+        assertThat(instance.getWorkers()).as("workers").hasSize(4).contains(worker4);
+    }
+
+    @Test
+    void defaultIsValid() {
+        Solution instance = new SolutionBuilder().build();
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            Validator validator = factory.getValidator();
+            Set<ConstraintViolation<Solution>> violations = validator.validate(instance);
+            assertThat(violations).as("constraint violations").isEmpty();
+        }
+    }
+}
